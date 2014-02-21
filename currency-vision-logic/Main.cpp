545338e82b1fdf94838bb56b3c55dcfe5470a8b1
kdnc <nuwanlanka@gmail.com>
2014-02-21 16:52:35 +0530

    OpenCV c++ application design
    - Added the edge detection code in to the main business logic code

--- currency-vision-logic/Main.cpp
+++ currency-vision-logic/Main.cpp
@@ -26,6 +26,11 @@ uchar cCTHue[NUM_COLOR_TYPES] =    {0,       0,      0,     0,     20,      30, 
 uchar cCTSat[NUM_COLOR_TYPES] =    {0,       0,      0,    255,   255,     255,     255,   255,   255,    255,     255};
 uchar cCTVal[NUM_COLOR_TYPES] =    {0,      255,    120,   255,   255,     255,     255,   255,   255,    255,     255};
 
+Mat src; Mat src_gray;
+int thresh = 100;
+int max_thresh = 255;
+RNG rng(12345);
+
 void saveImage(IplImage *imageShirtHSV)
 {
 	int h = imageShirtHSV->height;				// Pixel height
@@ -90,17 +95,98 @@ int getPixelColorType(int H, int S, int V)
 	return color;
 }
 
+void thresh_callback(int, void* )
+{
+  Mat threshold_output;
+  vector<vector<Point> > contours;
+  vector<Vec4i> hierarchy;
+
+  /// Detect edges using Threshold
+  threshold( src_gray, threshold_output, thresh, 255, THRESH_BINARY );
+
+  /// Find contours
+  findContours( threshold_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+  // Having CV_RETR_EXTERNAL instead of CV_RETR_TREE, will only return the outermost contours.
+  //findContours( threshold_output, contours, hierarchy, CV_RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+  /// Approximate contours to polygons + get bounding rects and circles
+  vector<vector<Point> > contours_poly( contours.size() );
+  vector<Rect> boundRect( contours.size() );
+  vector<Point2f>center( contours.size() );
+  vector<float>radius( contours.size() );
+
+  int largest_area=0;
+  int largest_contour_index=0;
+  Rect bounding_rect;
+
+  for( size_t i = 0; i < contours.size(); i++ ) { 
+	  double a=contourArea( contours[i],false);  //  Find the area of contour
+      if(a>largest_area){
+		largest_area=a;
+		largest_contour_index=i;                //Store the index of largest contour
+		bounding_rect=boundingRect(contours[i]); // Find the bounding rectangle for biggest contour
+      }
+   
+	  approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+      boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+      minEnclosingCircle( contours_poly[i], center[i], radius[i] );
+  }
+
+  /// Draw polygonal contour + bonding rects + circles
+  Mat drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
+  for( size_t i = 0; i< contours.size(); i++ ) {
+	 Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+	 //drawContours( src, contours_poly, (int)i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+	 //rectangle( src, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+	 //circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
+  }
+
+  Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+  //drawContours( src, contours,largest_contour_index, color, CV_FILLED, 8, hierarchy ); // Draw the largest contour using previously stored index.
+  rectangle(src, bounding_rect,  Scalar(0,255,0),1, 8,0);  
+
+  Mat croppedImage = src(bounding_rect);
+
+  /// Show in a window
+  namedWindow( "Contours", WINDOW_AUTOSIZE );
+  imshow( "Contours", src );
+  imshow( "Cropped Image", croppedImage );
+}
+
 int main(int argc, const char **argv)
 {
-	CommandLineParser parser(argc, argv, keys);
-	string infile = parser.get<std::string>("input");
-	string outdir = parser.get<std::string>("outdir");
+	//CommandLineParser parser(argc, argv, keys);
+	//string infile = parser.get<std::string>("input");
+	//string outdir = parser.get<std::string>("outdir");
 
-	char *strFileImage = new char[infile.size() + 1];
-	copy(infile.begin(), infile.end(), strFileImage);
-	strFileImage[infile.size()] = '\0'; // don't forget the terminating 0
+	//------------- EDGE DETECTION - BEGIN ---------------
 
-	//char *strFileImage = "20_new_note_1 (Medium)_cropped.jpg";	// default file
+	/// Load source image and convert it to gray
+	src = imread( "img4.jpg", 1 );
+
+	/// Convert image to gray and blur it
+	cvtColor( src, src_gray, COLOR_BGR2GRAY );
+	blur( src_gray, src_gray, Size(3,3) );
+
+	/// Create Window
+	const char* source_window = "Source";
+	namedWindow( source_window, WINDOW_AUTOSIZE );
+	imshow( source_window, src );
+
+	createTrackbar( " Threshold:", "Source", &thresh, max_thresh, thresh_callback );
+	thresh_callback( 0, 0 );
+
+	//------------- EDGE DETECTION - END   ---------------
+
+	
+
+	//------------- COLOR DETECTION - BEGIN ---------------
+
+	//char *strFileImage = new char[infile.size() + 1];
+	//copy(infile.begin(), infile.end(), strFileImage);
+	//strFileImage[infile.size()] = '\0'; // don't forget the terminating 0
+
+	char *strFileImage = "img4.jpg";	// default file
 	//char *strFileImage = "20_new_note_1 (Medium)_cropped.jpg";
 
 	IplImage *imageIn = cvLoadImage(strFileImage, CV_LOAD_IMAGE_UNCHANGED);
@@ -153,6 +239,7 @@ int main(int argc, const char **argv)
 	int percentage = initialConfidence * (tallyMaxCount * 100 / pixels);
 	cout << "Color of currency note: " << sCTypes[tallyMaxIndex] << " (" << percentage << "% confidence)." << endl << endl;
 
+	//------------- COLOR DETECTION - END ---------------
 
 	//------- Saving the image
 	saveImage(imageShirtHSV);
