773aea9771bdb7edf25e6e5623bfc60c79780217
kdnc <nuwanlanka@gmail.com>
2014-02-24 16:50:59 +0530

    currency-vision-web
    - Communicate with c++ business logic from php web server

--- currency-vision-web/web-service/currency-detect.php
+++ currency-vision-web/web-service/currency-detect.php
@@ -1,14 +1,30 @@
 <?php
+	//------- Temporary code to check the call from Android is received to server ----------
+	$file = 'output.txt';
+	// Open the file to get existing content
+	$current = file_get_contents($file);
+	// Append a new person to the file
+	//$current .= "John Smith\n";
+	$current .= rand() . " - test\n";
+	// Write the contents back to the file
+	file_put_contents($file, $current);
+	
+	// Save the received image file in server
+	// $base=$_REQUEST['image'];
+    // $binary=base64_decode($base);
+    // header('Content-Type: bitmap; charset=utf-8');
+    // $file = fopen('uploaded_image.jpg', 'wb');
+    // fwrite($file, $binary);
+    // fclose($file);
 
-// if (isset($_SERVER['HTTP_X_FILENAME'])) 
-// {
-	// $file = 'uploads/'.$_SERVER['HTTP_X_FILENAME'];
-	$file = '2.jpg';
-	file_put_contents("$file", file_get_contents("php://input"));
 
-	$cmd  = 'currency-vision-logic --input="'.$file.'" --outdir="'.dirname($file).'"';
+	$filename = '50_note_on_black_background_1_(Medium).jpg';
+	//file_put_contents("$filename", file_get_contents("php://input"));
+
+	$cmd  = 'currency-vision-logic8 --input="'.$filename.'" --outdir="'.dirname($filename).'"';
 	$last = exec($cmd, $out);
-
+	echo 'last - ' . $last;
+	echo '[out ]- ' . $out;
 	if (strpos($last, 'Error:') === false)
 	{
 		$res = array('success' => true);
@@ -24,13 +40,3 @@
 
 	header('Content-Type: application/json');
 	echo json_encode($res);
-// } 
-// else
-// {
-	// header('HTTP/1.1 404 Not found');
-	// header('Content-Type: application/json');
-	// echo json_encode(array(
-		// 'success' => false,
-		// 'msg' => '404 - The resource you requested is not found.'
-	// ));
-// }
