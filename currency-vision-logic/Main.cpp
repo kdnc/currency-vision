cb7c381bb524cd7507235a1bfdc4bc662ef8423a
kdnc <nuwanlanka@gmail.com>
2014-02-25 17:03:21 +0530

    currency-vision-logic Added SURF feature detection
    Added SURF Feature detection to the code. This showed real improvement
    on identifying the pattern objects.

--- currency-vision-logic/Main.cpp
+++ currency-vision-logic/Main.cpp
@@ -155,7 +155,6 @@ int main(int argc, const char **argv)
 {
 	ofstream a_file;
 	a_file.open("output.txt");
-	a_file << "test";
 	
 	CommandLineParser parser(argc, argv, keys);
 	string infile = parser.get<std::string>("input");
@@ -164,7 +163,7 @@ int main(int argc, const char **argv)
 	//------------- EDGE DETECTION - BEGIN ---------------
 
 	if(infile == "" && outdir == ""){
-		src = imread( "50_note_on_black_background_1_(Medium).jpg", 1 );
+		src = imread( "50_note_on_black_background_2_(Medium).jpg", 1 );
 	} else {
 		src = imread( infile, 1 );
 	}
@@ -182,8 +181,9 @@ int main(int argc, const char **argv)
 	Mat croppedImage = thresh_callback( 0, 0 );
 	
 	/// Show in a window
-	namedWindow( "Contours", WINDOW_AUTOSIZE );
-	imshow( "Contours - ", src );
+	//namedWindow( "Contours", WINDOW_AUTOSIZE );
+	//imshow( "Contours - ", src );
+	namedWindow( "Cropped Image - ", WINDOW_AUTOSIZE );
 	imshow( "Cropped Image - ", croppedImage );
 
 	//------------- EDGE DETECTION - END   ---------------
@@ -260,85 +260,119 @@ int main(int argc, const char **argv)
 
 	//------------- Flann Feature Matching - BEGIN---------------
 
-	  //if( argc != 3 )
-  //{ readme(); return -1; }
-  Mat img_1 = imread( "template.png", IMREAD_GRAYSCALE );
-  //Mat img_2 = imread( "50note1.jpg", IMREAD_GRAYSCALE );
-  Mat img_2 = Mat(croppedImage);
-  cvtColor( img_2, img_2, COLOR_BGR2GRAY );
+	Mat object = imread( "template.png", CV_LOAD_IMAGE_GRAYSCALE );
 
-  if( !img_1.data || !img_2.data )
-  { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
+    if( !object.data )
+    {
+        std::cout<< "Error reading object " << std::endl;
+        return -1;
+    }
 
-  //-- Step 1: Detect the keypoints using SURF Detector
-  int minHessian = 400;
+    //Detect the keypoints using SURF Detector
+    int minHessian = 500;
 
-  SurfFeatureDetector detector( minHessian );
+    SurfFeatureDetector detector( minHessian );
+    std::vector<KeyPoint> kp_object;
 
-  std::vector<KeyPoint> keypoints_1, keypoints_2;
+    detector.detect( object, kp_object );
 
-  detector.detect( img_1, keypoints_1 );
-  detector.detect( img_2, keypoints_2 );
+    //Calculate descriptors (feature vectors)
+    SurfDescriptorExtractor extractor;
+    Mat des_object;
 
-  //-- Step 2: Calculate descriptors (feature vectors)
-  SurfDescriptorExtractor extractor;
+    extractor.compute( object, kp_object, des_object );
 
-  Mat descriptors_1, descriptors_2;
+    FlannBasedMatcher matcher;
 
-  extractor.compute( img_1, keypoints_1, descriptors_1 );
-  extractor.compute( img_2, keypoints_2, descriptors_2 );
+    VideoCapture cap(0);
 
-  //-- Step 3: Matching descriptor vectors using FLANN matcher
-  FlannBasedMatcher matcher;
-  std::vector< DMatch > matches;
-  matcher.match( descriptors_1, descriptors_2, matches );
+    namedWindow("Good Matches");
 
-  double max_dist = 0; double min_dist = 100;
+    std::vector<Point2f> obj_corners(4);
 
-  //-- Quick calculation of max and min distances between keypoints
-  for( int i = 0; i < descriptors_1.rows; i++ )
-  { double dist = matches[i].distance;
-    if( dist < min_dist ) min_dist = dist;
-    if( dist > max_dist ) max_dist = dist;
-  }
+    //Get the corners from the object
+    obj_corners[0] = cvPoint(0,0);
+    obj_corners[1] = cvPoint( object.cols, 0 );
+    obj_corners[2] = cvPoint( object.cols, object.rows );
+    obj_corners[3] = cvPoint( 0, object.rows );
 
-  printf("-- Max dist : %f \n", max_dist );
-  printf("-- Min dist : %f \n", min_dist );
+    char key = 'a';
+    int framecount = 0;
+    //while (key != 27)
+    //{
+        //Mat frame = imread( "50.jpg", -1);;
+		Mat frame = Mat(croppedImage);
+        /*cap >> frame;
 
-  //-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
-  //-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
-  //-- small)
-  //-- PS.- radiusMatch can also be used here.
-  std::vector< DMatch > good_matches;
+        if (framecount < 5)
+        {
+            framecount++;
+            continue;
+        }*/
 
-  for( int i = 0; i < descriptors_1.rows; i++ )
-  { if( matches[i].distance <= max(2*min_dist, 0.02) )
-    { good_matches.push_back( matches[i]); }
-  }
+        Mat des_image, img_matches;
+        std::vector<KeyPoint> kp_image;
+        std::vector<vector<DMatch > > matches;
+        std::vector<DMatch > good_matches;
+        std::vector<Point2f> obj;
+        std::vector<Point2f> scene;
+        std::vector<Point2f> scene_corners(4);
+        Mat H;
+        Mat image;
 
-  //-- Draw only "good" matches
-  Mat img_matches;
-  drawMatches( img_1, keypoints_1, img_2, keypoints_2,
-               good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
-               vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+        cvtColor(frame, image, CV_RGB2GRAY);
 
-  //-- Show detected matches
-  imshow( "Good Matches - ", img_matches );
-  imwrite("img_matches.png", img_matches);
-  
-  for( int i = 0; i < (int)good_matches.size(); i++ )
-  { printf( "-- Good Match [%d] Keypoint 1: %d  -- Keypoint 2: %d  \n", i, good_matches[i].queryIdx, good_matches[i].trainIdx ); }
+        detector.detect( image, kp_image );
+        extractor.compute( image, kp_image, des_image );
+
+        matcher.knnMatch(des_object, des_image, matches, 2);
+
+        for(int i = 0; i < min(des_image.rows-1,(int) matches.size()); i++) //THIS LOOP IS SENSITIVE TO SEGFAULTS
+        {
+            if((matches[i][0].distance < 0.6*(matches[i][1].distance)) && ((int) matches[i].size()<=2 && (int) matches[i].size()>0))
+            {
+                good_matches.push_back(matches[i][0]);
+            }
+        }
+
+        //Draw only "good" matches
+        drawMatches( object, kp_object, image, kp_image, good_matches, img_matches, Scalar::all(-1), Scalar::all(-1), vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+
+        if (good_matches.size() >= 4)
+        {
+            for( int i = 0; i < good_matches.size(); i++ )
+            {
+                //Get the keypoints from the good matches
+                obj.push_back( kp_object[ good_matches[i].queryIdx ].pt );
+                scene.push_back( kp_image[ good_matches[i].trainIdx ].pt );
+            }
+
+            H = findHomography( obj, scene, CV_RANSAC );
+
+            perspectiveTransform( obj_corners, scene_corners, H);
+
+            //Draw lines between the corners (the mapped object in the scene image )
+            line( img_matches, scene_corners[0] + Point2f( object.cols, 0), scene_corners[1] + Point2f( object.cols, 0), Scalar(0, 255, 0), 4 );
+            line( img_matches, scene_corners[1] + Point2f( object.cols, 0), scene_corners[2] + Point2f( object.cols, 0), Scalar( 0, 255, 0), 4 );
+            line( img_matches, scene_corners[2] + Point2f( object.cols, 0), scene_corners[3] + Point2f( object.cols, 0), Scalar( 0, 255, 0), 4 );
+            line( img_matches, scene_corners[3] + Point2f( object.cols, 0), scene_corners[0] + Point2f( object.cols, 0), Scalar( 0, 255, 0), 4 );
+        }
+
+		a_file << "| Good matches count - ";
+		a_file << good_matches.size();
+        //Show detected matches
+        imshow( "Good Matches", img_matches );
 
 
   //------------- Flann Feature Matching - END---------------
 
 	//------- Saving the image
-	saveImage(imageShirtHSV);
+	//saveImage(imageShirtHSV);
 	//------- Saving the image
 	cout << "This is the data which gets returned to the php server code";
-	a_file << "Then Came here";
-	cvNamedWindow("Shirt", 1);
-    cvShowImage("Shirt", imageShirtHSV);
+
+	//cvNamedWindow("Shirt", 1);
+    //cvShowImage("Shirt", imageShirtHSV);
 	a_file.close();
 	if(infile == "" && outdir == ""){
 		cvWaitKey();
