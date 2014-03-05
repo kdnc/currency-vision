dab07c92ece8b28a8411a33eb20329ecd52dcb42
kdnc <nuwanlanka@gmail.com>
2014-03-05 15:20:01 +0530

    currency-vision-mobile UI Design for Demo
    - Updated remote server call to use AsyncTask
    - Added progress icon for the waiting time in remote server call
    - Added TableLayout to display buttons for the progress meeting 1 demo
    - Added window title bar with only text

--- currency-vision-mobile/src/com/currencyvision/MainActivity.java
+++ currency-vision-mobile/src/com/currencyvision/MainActivity.java
@@ -1,20 +1,26 @@
 package com.currencyvision;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
-import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -26,16 +32,6 @@ public class MainActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
 		setContentView(R.layout.activity_main);
 		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.window_title);
-//		final boolean customTitleSupported = requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
-//		if ( customTitleSupported ) {
-//	        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.mytitle);
-//	    }
-//
-//	    final TextView myTitleText = (TextView) findViewById(R.id.myTitle);
-//	    if ( myTitleText != null ) {
-//	        myTitleText.setText("========= NEW TITLE ==========");
-//	        myTitleText.setBackgroundColor(Color.GREEN);
-//	    }
 	}
 
 	@Override
@@ -46,72 +42,108 @@ public class MainActivity extends Activity {
 	}
 	
 	public void sendMessage(View view) {
-	    Intent intent = new Intent(this, CameraDemoActivity.class);
+//	    Intent intent = new Intent(this, CameraDemoActivity.class);
 //	    EditText editText = (EditText) findViewById(R.id.edit_message);
 //	    String message = editText.getText().toString();
 //	    intent.putExtra(EXTRA_MESSAGE, message);
-	    startActivity(intent);
+//	    startActivity(intent);
 		
 //		Intent intent = new Intent(this, AndroidTextToSpeechActivity.class);
 //	    startActivity(intent);
 	    
 //	    sendCapturedImageToServer();
+		
+		String serverURL = "http://10.0.2.2/currency-vision/web-service/currency-detect.php";
+        // Use AsyncTask execute Method To Prevent ANR Problem
+        new LongOperation().execute(serverURL);
 	}
 	
-	private void sendCapturedImageToServer() {
-		Thread trd = new Thread(new Runnable(){
-	    	  @Override
-	    	  public void run(){
-//	            String url = "http://10.200.124.41/currency-vision/web-service/test.php";
-	            String url = "http://10.0.2.2/currency-vision/web-service/currency-detect.php";
+	private void openAlert(String result) {
+		 AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+	     
+		 alertDialogBuilder.setTitle("Currency Note Result");
+		 alertDialogBuilder.setMessage(result);
 
-    	        try {
-//    	        	ByteArrayOutputStream stream = new ByteArrayOutputStream();
-//    	        	byte [] byte_arr = stream.toByteArray();
-//    	        	String image_str = Base64.encodeBytes(byte_arr);
-//    	        	ArrayList<NameValuePair> nameValuePairs = new  ArrayList<NameValuePair>();
-//                  
-//    	        	nameValuePairs.add(new BasicNameValuePair("image",image_str));
-//                  
-    	            HttpClient httpclient = new DefaultHttpClient();
-    	            HttpPost httppost = new HttpPost(url);
-//    	          	httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-    	            HttpResponse response = httpclient.execute(httppost);
-					String the_string_response = convertResponseToString(response);
-					System.out.println("success");
-    	        }catch(Exception e){
-                  System.out.println("Error in http connection "+e.toString());
-    	        }
-	    	  }
-      	});
-      	trd.start();
+		 alertDialogBuilder.setPositiveButton("OK",new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog,int id) {
+				dialog.cancel();
+			}
+		 });
+		 
+		 AlertDialog alertDialog = alertDialogBuilder.create();
+		 alertDialog.show();
 	}
 	
-	public String convertResponseToString(HttpResponse response) throws IllegalStateException, IOException{
-   	 InputStream inputStream;
-       String res = "";
-       StringBuffer buffer = new StringBuffer();
-       inputStream = response.getEntity().getContent();
-       int contentLength = (int) response.getEntity().getContentLength(); //getting content length…..
-       if (contentLength < 0){
-       } else {
-              byte[] data = new byte[512];
-              int len = 0;
-              try {
-                  while (-1 != (len = inputStream.read(data)) ){
-                      buffer.append(new String(data, 0, len)); //converting to string and appending  to stringbuffer…..
-                  }
-              } catch (IOException e) {
-                  e.printStackTrace();
-              }
-              try {
-                  inputStream.close(); // closing the stream…..
-              } catch (IOException e) {
-                  e.printStackTrace();
-              }
-              res = buffer.toString();     // converting stringbuffer to string…..
-       }
-       return res;
-  }
-	
+	private class LongOperation  extends AsyncTask<String, Void, Void> {
+        
+        // Required initialization
+        private String content;
+        private String error = null;
+        private ProgressDialog Dialog = new ProgressDialog(MainActivity.this);
+        String data =""; 
+         
+        protected void onPreExecute() {
+            // NOTE: You can call UI Element here.
+            //Start Progress Dialog (Message)
+            Dialog.setMessage("Please wait..");
+            Dialog.show();
+            try{
+                // Set Request parameter
+                data +="&" + URLEncoder.encode("data", "UTF-8") + "=20rupee";
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } 
+        }
+  
+        // Call after onPreExecute method
+        protected Void doInBackground(String... urls) {
+            // Make Post Call To Web Server
+            BufferedReader reader=null;
+            // Send data 
+            try{ 
+               // Defined URL  where to send data
+               URL url = new URL(urls[0]);
+                  
+              // Send POST data request
+              URLConnection conn = url.openConnection(); 
+              conn.setDoOutput(true); 
+              OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream()); 
+              wr.write( data ); 
+              wr.flush(); 
+           
+              // Get the server response 
+              reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+              StringBuilder sb = new StringBuilder();
+              String line = null;
+             
+	          // Read Server Response
+	          while((line = reader.readLine()) != null) {
+	             // Append server response in string
+	             sb.append(line);
+	          }
+	          // Append Server Response To Content String 
+              content = sb.toString();
+            } catch(Exception ex) {
+                error = ex.getMessage();
+            } finally {
+                try{
+                    reader.close();
+                } catch(Exception ex) {
+                }
+            }
+            return null;
+        }
+          
+        protected void onPostExecute(Void unused) {
+            // NOTE: You can call UI Element here.
+            // Close progress dialog
+            Dialog.dismiss();
+            
+            if (error != null) {
+            	Log.e("CV Error", "Error on the server call");
+            } else {
+                openAlert(content);
+            }
+        }
+    }
 }
