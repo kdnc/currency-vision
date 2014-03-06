ecdbad0e5a76aff945e3115939150b2464697e6a
kdnc <nuwanlanka@gmail.com>
2014-03-06 16:07:00 +0530

    currency-vision-web Organize code for demo
    - Organize the code to get images from resources folder and send the
    output message to a file in output folder

--- currency-vision-web/web-service/currency-detect.php
+++ currency-vision-web/web-service/currency-detect.php
@@ -1,13 +1,12 @@
 <?php
 	//------- Temporary code to check the call from Android is received to server ----------
-	$file = 'output.txt';
+	//$file = 'output/output2.txt';
 	// Open the file to get existing content
-	$current = file_get_contents($file);
+	//$current = file_get_contents($file);
 	// Append a new person to the file
-	//$current .= "John Smith\n";
-	$current .= rand() . " - test\n";
+	//$current .= rand() . " - test\n";
 	// Write the contents back to the file
-	file_put_contents($file, $current);
+	//file_put_contents($file, $current);
 	
 	// Save the received image file in server
 	// $base=$_REQUEST['image'];
@@ -17,31 +16,33 @@
     // fwrite($file, $binary);
     // fclose($file);
 
-	$filename = 'note_20_1';
+	$filename = 'resources/';
 	
 	if(isset($_GET['data'])){
-		$filename = $_GET['data'];
+		$filename .= $_GET['data'];
+	} else {
+		$filename .= 'note_20_1';
 	}
 	$filename .= '.jpg';
 	
 	//file_put_contents("$filename", file_get_contents("php://input"));
 
-	$cmd  = 'currency-vision-logic6 --input="'.$filename.'" --outdir="'.dirname($filename).'"';
+	$cmd  = 'currency-vision-logic --input="'.$filename.'" --outdir="'.dirname($filename).'"';
 	$last = exec($cmd, $out);
 	//echo 'last - ' . $last;
 	//echo '[out ]- ' . $out;
-	if (strpos($last, 'Error:') === false)
-	{
-		$res = array('success' => true);
+	// if (strpos($last, 'Error:') === false)
+	// {
+		// $res = array('success' => true);
 
-		foreach ($out as $i => $line)
-		{
-			if (preg_match('/\d+,\s*([^\s]+)\s+\((\d+)x(\d+)\)/i', $line, $m))
-				$res['images'][] = array('src' => $m[1], 'width' => $m[2], 'height' => $m[3]);
-		}
-	}
-	else
-		$res = array('success' => false, 'msg' => $last);
+		// foreach ($out as $i => $line)
+		// {
+			// if (preg_match('/\d+,\s*([^\s]+)\s+\((\d+)x(\d+)\)/i', $line, $m))
+				// $res['images'][] = array('src' => $m[1], 'width' => $m[2], 'height' => $m[3]);
+		// }
+	// }
+	// else
+		// $res = array('success' => false, 'msg' => $last);
 
 	header('Content-Type: application/json');
 	//echo json_encode($res);
