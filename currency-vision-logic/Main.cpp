e46922209c11c5183e0ae9e58fb8e0f04703d9e7
kdnc <nuwanlanka@gmail.com>
2014-03-11 17:29:51 +0530

    currency-vision-logic Modularize Pattern Matching
    - Modularized pattern matching code in a separate cpp file
    "PatternMatchingUtil"
    - Added the test image data

--- currency-vision-logic/Main.cpp
+++ currency-vision-logic/Main.cpp
@@ -11,6 +11,8 @@
 #include <opencv\highgui.h>
 #include "opencv2/nonfree/features2d.hpp"
 
+#include "PatternMatchingUtil.h";
+
 using namespace std;
 using namespace cv;
 
@@ -163,49 +165,49 @@ int main(int argc, const char **argv)
 
 	//------------- EDGE DETECTION - BEGIN ---------------
 
-	//if(infile == "" && outdir == ""){
-	//	src = imread( "50_note_on_black_background_2_(Medium).jpg", 1 );
-	//} else {
-	//	src = imread( infile, 1 );
-	//}
+	if(infile == "" && outdir == ""){
+		infile = "resources/20_new_note_1 (Medium).jpg";
+	}
+	output += "Note Location - " + infile + "|";
+	src = imread( infile, 1 );
+	
+	/// Convert image to gray and blur it
+	cvtColor( src, src_gray, COLOR_BGR2GRAY );
+	blur( src_gray, src_gray, Size(3,3) );
 
-	///// Convert image to gray and blur it
-	//cvtColor( src, src_gray, COLOR_BGR2GRAY );
-	//blur( src_gray, src_gray, Size(3,3) );
-
-	///// Create Window
-	//const char* source_window = "Source";
+	/// Create Window
+	const char* source_window = "Source";
 	//namedWindow( source_window, WINDOW_AUTOSIZE );
 	//imshow( source_window, src );
 
-	////createTrackbar( " Threshold:", "Source", &thresh, max_thresh, thresh_callback );
-	//Mat croppedImage = thresh_callback( 0, 0 );
-	//
-	///// Show in a window
-	////namedWindow( "Contours", WINDOW_AUTOSIZE );
-	////imshow( "Contours - ", src );
-	//namedWindow( "Cropped Image - ", WINDOW_AUTOSIZE );
-	//imshow( "Cropped Image - ", croppedImage );
+	//createTrackbar( " Threshold:", "Source", &thresh, max_thresh, thresh_callback );
+	Mat croppedImage = thresh_callback( 0, 0 );
+	
+	/// Show in a window
+	//namedWindow( "Contours", WINDOW_AUTOSIZE );
+	//imshow( "Contours - ", src );
+	namedWindow( "Cropped Image - ", WINDOW_AUTOSIZE );
+	imshow( "Cropped Image - ", croppedImage );
 
 	//------------- EDGE DETECTION - END   ---------------
 
 	
 	//------------- COLOR DETECTION - BEGIN ---------------
 
-	if(infile == "" && outdir == ""){
-		infile = "resources/note_20_1.jpg";
-	}
-	output += "Note Location - " + infile + "|";
+	//if(infile == "" && outdir == ""){
+	//	infile = "resources/note_20_1.jpg";
+	//}
+	//output += "Note Location - " + infile + "|";
 
-	char *strFileImage = new char[infile.size() + 1];
-	copy(infile.begin(), infile.end(), strFileImage);
-	strFileImage[infile.size()] = '\0'; // don't forget the terminating 0
+	//char *strFileImage = new char[infile.size() + 1];
+	//copy(infile.begin(), infile.end(), strFileImage);
+	//strFileImage[infile.size()] = '\0'; // don't forget the terminating 0
 
 	//char *strFileImage = "img4.jpg";	// default file
 	//char *strFileImage = "20_new_note_1 (Medium)_cropped.jpg";
 
-	IplImage *imageIn = cvLoadImage(strFileImage, CV_LOAD_IMAGE_UNCHANGED);
-	//IplImage *imageIn = cvCloneImage(&(IplImage)croppedImage);
+	//IplImage *imageIn = cvLoadImage(strFileImage, CV_LOAD_IMAGE_UNCHANGED);
+	IplImage *imageIn = cvCloneImage(&(IplImage)croppedImage);
 
 	IplImage *imageCurrency = cvCloneImage(imageIn);
 
@@ -272,117 +274,37 @@ int main(int argc, const char **argv)
 	a_file << percentage;
 	a_file << "% confidence).";
 
-	saveImage(imageCurrencyHSV);
+	//saveImage(imageCurrencyHSV);
 	
 	//------------- COLOR DETECTION - END ---------------
 
 	//------------- Flann Feature Matching - BEGIN---------------
 
-	//Mat object = imread( "template.png", CV_LOAD_IMAGE_GRAYSCALE );
+	
+	string templates[] = {"50template","20template","100template"};
+	int good_matches = 0;
+	string match_note = "";
+	int size = sizeof(templates)/sizeof(string);
+	PatternMatchingUtil patternMatcherUtil;
 
- //   if( !object.data )
- //   {
- //       std::cout<< "Error reading object " << std::endl;
- //       return -1;
- //   }
+	for(int i=0; i<size; i++){
+		int matches_count = patternMatcherUtil.detectPattern(croppedImage, templates[i]);
+		if(matches_count > good_matches){
+			good_matches = matches_count;
+			match_note = templates[i];
+		}
+	}
+	a_file << match_note;
+	a_file << "| Good matchess count - ";
+	a_file << good_matches;
+	output += match_note;
+	output += "| Good matches counts - ";
+	output += to_string(good_matches);
 
- //   //Detect the keypoints using SURF Detector
- //   int minHessian = 500;
+	 //   namedWindow("Good Matches");
+	 //       //Show detected matches
+	 //   imshow( "Good Matches", img_matches );
 
- //   SurfFeatureDetector detector( minHessian );
- //   std::vector<KeyPoint> kp_object;
-
- //   detector.detect( object, kp_object );
-
- //   //Calculate descriptors (feature vectors)
- //   SurfDescriptorExtractor extractor;
- //   Mat des_object;
-
- //   extractor.compute( object, kp_object, des_object );
-
- //   FlannBasedMatcher matcher;
-
- //   VideoCapture cap(0);
-
- //   namedWindow("Good Matches");
-
- //   std::vector<Point2f> obj_corners(4);
-
- //   //Get the corners from the object
- //   obj_corners[0] = cvPoint(0,0);
- //   obj_corners[1] = cvPoint( object.cols, 0 );
- //   obj_corners[2] = cvPoint( object.cols, object.rows );
- //   obj_corners[3] = cvPoint( 0, object.rows );
-
- //   char key = 'a';
- //   int framecount = 0;
- //   //while (key != 27)
- //   //{
- //       //Mat frame = imread( "50.jpg", -1);;
-	//	Mat frame = Mat(croppedImage);
- //       /*cap >> frame;
-
- //       if (framecount < 5)
- //       {
- //           framecount++;
- //           continue;
- //       }*/
-
- //       Mat des_image, img_matches;
- //       std::vector<KeyPoint> kp_image;
- //       std::vector<vector<DMatch > > matches;
- //       std::vector<DMatch > good_matches;
- //       std::vector<Point2f> obj;
- //       std::vector<Point2f> scene;
- //       std::vector<Point2f> scene_corners(4);
- //       Mat H;
- //       Mat image;
-
- //       cvtColor(frame, image, CV_RGB2GRAY);
-
- //       detector.detect( image, kp_image );
- //       extractor.compute( image, kp_image, des_image );
-
- //       matcher.knnMatch(des_object, des_image, matches, 2);
-
- //       for(int i = 0; i < min(des_image.rows-1,(int) matches.size()); i++) //THIS LOOP IS SENSITIVE TO SEGFAULTS
- //       {
- //           if((matches[i][0].distance < 0.6*(matches[i][1].distance)) && ((int) matches[i].size()<=2 && (int) matches[i].size()>0))
- //           {
- //               good_matches.push_back(matches[i][0]);
- //           }
- //       }
-
- //       //Draw only "good" matches
- //       drawMatches( object, kp_object, image, kp_image, good_matches, img_matches, Scalar::all(-1), Scalar::all(-1), vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
-
- //       if (good_matches.size() >= 4)
- //       {
- //           for( int i = 0; i < good_matches.size(); i++ )
- //           {
- //               //Get the keypoints from the good matches
- //               obj.push_back( kp_object[ good_matches[i].queryIdx ].pt );
- //               scene.push_back( kp_image[ good_matches[i].trainIdx ].pt );
- //           }
-
- //           H = findHomography( obj, scene, CV_RANSAC );
-
- //           perspectiveTransform( obj_corners, scene_corners, H);
-
- //           //Draw lines between the corners (the mapped object in the scene image )
- //           line( img_matches, scene_corners[0] + Point2f( object.cols, 0), scene_corners[1] + Point2f( object.cols, 0), Scalar(0, 255, 0), 4 );
- //           line( img_matches, scene_corners[1] + Point2f( object.cols, 0), scene_corners[2] + Point2f( object.cols, 0), Scalar( 0, 255, 0), 4 );
- //           line( img_matches, scene_corners[2] + Point2f( object.cols, 0), scene_corners[3] + Point2f( object.cols, 0), Scalar( 0, 255, 0), 4 );
- //           line( img_matches, scene_corners[3] + Point2f( object.cols, 0), scene_corners[0] + Point2f( object.cols, 0), Scalar( 0, 255, 0), 4 );
- //       }
-
-	//	a_file << "| Good matches count - ";
-	//	a_file << good_matches.size();
-	//	output += "| Good matches count - ";
-	//	output += to_string(good_matches.size());
-
- //       //Show detected matches
- //       imshow( "Good Matches", img_matches );
 
 
   //------------- Flann Feature Matching - END---------------
@@ -403,7 +325,7 @@ int main(int argc, const char **argv)
 	}
 
 	// Free resources.
-	delete[] strFileImage;
+	//delete[] strFileImage;
 	cvReleaseImage(&imageCurrencyHSV);
     cvReleaseImage(&imageCurrency);
     cvReleaseImage(&imageIn);
---
+++ currency-vision-logic/PatternMatchingUtil.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <cstdio>	// Used for "printf"
+#include <string>	// Used for C++ strings
+#include <iostream>	// Used for C++ cout print statements
+#include <fstream>
+//#include <cmath>	// Used to calculate square-root for statistics
+
+// Include OpenCV libraries
+#include <opencv\cv.h>
+#include <opencv\cvaux.h>
+#include <opencv\cxcore.h>
+#include <opencv\highgui.h>
+#include "opencv2/nonfree/features2d.hpp"
+
+class PatternMatchingUtil
+{
+	public:
+		int detectPattern(cv::Mat croppedImage, std::string templateName);
+};
+
